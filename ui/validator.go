9fd82dd1762359c86258d7e0c12e0ccf6f45bc45
Ionut Traistaru <ionut.m.traistaru@gmail.com>
2025-03-12 19:27:57 +0200

    refactor: increase maximum note name length to 40 characters

--- ui/validator.go
+++ ui/validator.go
@@ -2,10 +2,13 @@ package ui
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/charmbracelet/huh"
 )
+
+const maxNoteNameLength = 40
 
 func validateNoteName(input *huh.Input) (string, error) {
 	value := input.GetValue().(string)
@@ -15,8 +18,8 @@ func validateNoteName(input *huh.Input) (string, error) {
 		return "", errors.New("name cannot be empty")
 	}
 
-	if len(value) > 20 {
-		return "", errors.New("name cannot be longer than 20 characters")
+	if len(value) > maxNoteNameLength {
+		return "", fmt.Errorf("name cannot be longer than %d characters", maxNoteNameLength)
 	}
 
 	return value, nil
